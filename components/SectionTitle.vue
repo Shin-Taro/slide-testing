a0c4dae9c4b3e1495f096c046d3ae8328c8fc7f7
ShintaroTakahashi <shintaro_takahashi@bita.jp>
2024-07-11 06:18:24 +0900

    feat: adjust styles

--- components/SectionTitle.vue
+++ components/SectionTitle.vue
@@ -1,7 +1,13 @@
 <template>
   <h1
-    class="text-font text-sectionTitle bg-subBackground rounded-r-[20px] border-l-8 border-solid border-main p-3"
+    class="text-font text-sectionTitle bg-subBackground rounded-r-[20px] border-l-8 border-solid p-3"
   >
     <slot />
   </h1>
 </template>
+
+<style scoped>
+h1 {
+  border-color: var(--color-myPrimary);
+}
+</style>
--- uno.config.ts
+++ uno.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "unocss";
 
 export default defineConfig({
   shortcuts: {
-    "bg-main": "bg-background text-font dark:bg-background dark:text-font",
+    "bg-primary": "bg-background text-font dark:bg-background dark:text-font",
   },
   theme: {
     colors: {
       background: "#272727",
       subBackground: "#3c3c3c",
       font: "#ffffff",
-      main: "#148570",
+      myPrimary: "#148570",
       accent: "#890303",
     },
     fontSize: {
